4138145c9dafba05ac7b7f3a4e29c25f6b7ce755
David <david@ixulabs.com>
2021-05-22 12:46:16 -0500

    Sección 6: Vistas y Vistas Genéricas de Django Rest FrameWork

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .apiviews import ProductoList, ProductoDetalle
+from .apiviews import ProductoList, ProductoDetalle, CategoriaList, SubCategoriaList
  
 urlpatterns = [
     path('v1/productos/', ProductoList.as_view(), name='producto_list' ),
     path('v1/productos/<int:pk>', ProductoDetalle.as_view(), name='producto_detalle' ),
+    path('v1/categorias/', CategoriaList.as_view(), name='categoria_list' ),
+    path('v1/subcategorias/', SubCategoriaList.as_view(), name='subcategoria_list' ),
 ]
